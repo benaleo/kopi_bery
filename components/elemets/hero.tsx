f97c1c6a81148be97d67986019a7f3b80dbe9e09
benaleobayu <benaleobs@gmail.com>
2025-01-12 16:19:42 +0700

    first commit

---
+++ components/elemets/hero.tsx
@@ -0,0 +1,10 @@
+// @flow
+import * as React from 'react';
+
+export default function Hero() {
+    return (
+        <div className="w-full h-screen bg-slate-950">
+            Hero
+        </div>
+    );
+};
